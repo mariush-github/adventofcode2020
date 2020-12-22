f18c01df8af52868c0fd022a6f471d7142a025a3
mariush-github <mariush@helpedia.com>
2020-12-22 23:11:48 +0200

    Day 22

--- day22.php
+++ day22.php
@@ -1,15 +1,15 @@
 <?php
 
 $players = [];
-$players[0] = explode(',','15,31,26,4,36,30,43,39,50,21,25,46,6,44,12,20,23,9,48,11,16,42,17,13,10');
-$players[1] = explode(',','34,49,19,24,45,28,7,41,18,38,2,3,33,14,35,40,32,47,22,29,8,37,5,1,27');
+$players[0] = [15,31,26,4,36,30,43,39,50,21,25,46,6,44,12,20,23,9,48,11,16,42,17,13,10];
+$players[1] = [34,49,19,24,45,28,7,41,18,38,2,3,33,14,35,40,32,47,22,29,8,37,5,1,27];
 
 $continue=true;
 $turns = 0;
 while ($continue=true) {
     $turns++;
-    $a = intval(array_shift($players[0]));
-    $b = intval(array_shift($players[1]));
+    $a = array_shift($players[0]);
+    $b = array_shift($players[1]);
     if (($a==0)||($b==0))  {
         $continue = FALSE;
         break;
@@ -22,17 +22,17 @@ while ($continue=true) {
             array_push($players[1],$a);
         }
         echo "\n Turn $turns :";
-        echo "\n 0: ".$a;
-        echo "\n 1: ".$b;
-        echo "\n 0: ".json_encode($players[0]);
-        echo "\n 1: ".json_encode($players[1]);
+        echo "\n 1: ".$a;
+        echo "\n 2: ".$b;
+        echo "\n 1: ".json_encode($players[0]);
+        echo "\n 2: ".json_encode($players[1]);
         echo "\n";
     }
     if (count($players[0])==0) break;
     if (count($players[1])==0) break;
 }
 $result = (count($players[0])>0) ? $players[0] : $players[1];
-//var_dump($result);
+
 $j = 0;
 $total = 0;
 for ($i=count($result)-1;$i>=0;$i--) {
